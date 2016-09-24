5556e49a438ea87233f3ba8e87f6fbeae1bdefc8
rawffal <ccccplz@hotmail.com>
2016-09-24 00:33:11 -0700

    Added a stage. Player can now jump from the ground and collect a key to exit out of the door.

---
+++ Assets/Scripts/DoorController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class DoorController : MonoBehaviour
+{
+
+    bool hasKey;
+    bool canExit;
+
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.K) && hasKey && canExit) Destroy(this.gameObject);
+        if (canExit)
+        {
+            GetComponent<MeshRenderer>().materials[0].color = new Color(0, 255, 0, 0);
+        }
+        else
+        {
+            GetComponent<MeshRenderer>().materials[0].color = new Color(255, 0, 0, 0);
+        }
+
+    }
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        hasKey = coll.gameObject.GetComponent<PlayerController>().hasKey;
+        if (hasKey) canExit = true;
+    }
+
+    void OnTriggerExit2D(Collider2D coll)
+    {
+        if (hasKey) canExit = !canExit;
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,61 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    [SerializeField]
+    float moveSpeed;
+    [SerializeField]
+    float jumpHeight;
+
+    float dx;
+    float dy;
+    bool grounded;
+    bool canJump;
+
+    [SerializeField]
+    LayerMask whatIsGround;
+    [SerializeField]
+    Transform groundCircle;
+    [SerializeField]
+    float groundRadius;
+    bool ground;
+
+    public bool hasKey;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        dx = Input.GetAxis("Horizontal");
+        ground = Physics2D.OverlapCircle(groundCircle.position, groundRadius, whatIsGround);
+        if (Input.GetKeyDown(KeyCode.J) && ground)
+        {
+            canJump = true;
+        }
+    }
+
+    void FixedUpdate()
+    {
+        GetComponent<Rigidbody2D>().velocity = new Vector2(dx * moveSpeed, GetComponent<Rigidbody2D>().velocity.y);
+        if (canJump)
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(GetComponent<Rigidbody2D>().velocity.x, jumpHeight);
+            canJump = false;
+        }
+    }
+
+
+    void OnTriggerEnter2D(Collider2D coll)
+    {
+        if (coll.gameObject.CompareTag("Key"))
+        {
+            Destroy(coll.gameObject);
+            hasKey = true;
+        }
+    }
+}
